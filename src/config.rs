a252fdb7adff55f4d18ae4d0b1e63f9329364123
gowfqk <45060176+gowfqk@users.noreply.github.com>
2025-08-01 17:01:12 +0800

    修改服务器地址和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.deshuai.top"];
+pub const RS_PUB_KEY: &str = "iX24yM7eConytud9YSInVdeU3syPIHJodVl9+ZKIz20=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
